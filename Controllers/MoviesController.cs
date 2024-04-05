07fa4977b94e2e8fc78ba8abf3ba70ba349f57f5
olegbordilovskiy <vanasilvergoldsssk@mail.ru>
2024-04-05 18:17:43 +0300

    fixed viewmodel structure

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,32 +5,43 @@ using WebMovie.ViewModels;
 
 namespace WebMovie.Controllers
 {
-    public class MoviesController : Controller
-    {
-        private readonly  IMoviesService _moviesService;
-        private readonly IRatingsService _ratingsService;
-        private readonly IDirectorsService _directorsService;
+	public class MoviesController : Controller
+	{
+		private readonly IMoviesService _moviesService;
+		private readonly IRatingsService _ratingsService;
+		private readonly IDirectorsService _directorsService;
 		private readonly IRolesService _rolesService;
 		public MoviesController(IMoviesService moviesService, IRatingsService ratingsService, IDirectorsService directorsService, IRolesService rolesService)
-        {
-            _moviesService = moviesService;   
-            _ratingsService = ratingsService;
-            _directorsService = directorsService;
-            _rolesService = rolesService;
-        }
+		{
+			_moviesService = moviesService;
+			_ratingsService = ratingsService;
+			_directorsService = directorsService;
+			_rolesService = rolesService;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-			// transfer only the first 250
-			var moviesVM = new MoviesVM();
-            moviesVM.Movies = await _moviesService.GetAll();
-            moviesVM.Ratings = await _ratingsService.GetAll();
-           
-            return View(moviesVM);
-        }
-        public async Task<IActionResult> Create()
-        {
-            return View();
-        }
-    }
+		public async Task<IActionResult> Index()
+		{
+			var movies = await _moviesService.GetAll();
+			var ratings = await _ratingsService.GetAll();
+
+			var moviesVM = new List<MoviesVM>();
+
+			foreach (var movie in movies)
+			{
+				var rating = ratings.FirstOrDefault(r => r.MovieId == movie.Id);
+
+				moviesVM.Add(new MoviesVM
+				{
+					Movie = movie,
+					Rating = rating,
+				});
+			}
+
+			return View(moviesVM);
+		}
+		public async Task<IActionResult> Create()
+		{
+			return View();
+		}
+	}
 }
--- ViewModels/MoviesVM.cs
+++ ViewModels/MoviesVM.cs
@@ -4,10 +4,16 @@ namespace WebMovie.ViewModels
 {
 	public class MoviesVM
 	{
-		public IEnumerable<Movie> Movies { get; set; }
-		public IEnumerable<Rating> Ratings { get; set; }
+		//public IEnumerable<Movie> Movies { get; set; }
+		//public IEnumerable<Rating> Ratings { get; set; }
+		//public IEnumerable<Director> Directors { get; set; }
+		//public IEnumerable<KnownFor> KnownFors { get; set; }
+		//public IEnumerable<Writer> Writers { get; set; }
+		//public IEnumerable<Role> Roles { get; set; }
+		//public IEnumerable<Producer> Producers { get; set; }
+		public Movie Movie { get; set; }
+		public Rating Rating { get; set; }
 		public IEnumerable<Director> Directors { get; set; }
-		public IEnumerable<KnownFor> KnownFors { get; set; }
 		public IEnumerable<Writer> Writers { get; set; }
 		public IEnumerable<Role> Roles { get; set; }
 		public IEnumerable<Producer> Producers { get; set; }
